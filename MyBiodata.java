63bf835b337c1b425a943daa09dae5180fe922a2
CresentCrysy <raymondevtant@gmail.com>
2024-09-04 17:04:30 +0700

    Bio

--- MyBiodata.java
+++ MyBiodata.java
@@ -1,8 +1,9 @@
 public class MyBiodata {
     public static void main(String[] args) {
         String[] biodata= {"Raymon Devtant,", "Dari Situbondo,", "18"};
+        System.out.println("Perkenalkan saya: ");
         for (int i=0; i<3; i++) {
-            System.out.println("Perkenalkan Saya"+biodata[i]);
+            System.out.println(biodata[i]);
         }
     }
 }
