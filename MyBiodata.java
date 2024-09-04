45a0cc31c7ff3220ef0fc1eea2fee86dcfef14a1
CresentCrysy <raymondevtant@gmail.com>
2024-09-04 15:28:21 +0700

    First commit

---
+++ MyBiodata.java
@@ -0,0 +1,8 @@
+public class MyBiodata {
+    public static void main(String[] args) {
+        String[] biodata= {"Raymon Devtant", "18", "Situbondo"};
+        for (int i=0; i<3; i++) {
+            System.out.println(biodata[i]);
+        }
+    }
+}
