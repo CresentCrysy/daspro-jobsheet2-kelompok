64200424cb0a54ac1aece947c40b6b0157159e3d
FikarBahrul <Fikarbahrul@gmail.com>
2024-09-04 20:03:44 +0700

    firet commit

--- MyBiodataFikar.java
+++ MyBiodataFikar.java
@@ -1,4 +1,4 @@
-public class MyBiodata {
+public class MyBiodataFikar {
     public static void main(String[] args) {
         String[] biodata= {"Fikar Bahrul Santoso,", "18,", "Dari Pandaan"};
         for (int i=0; i<3; i++) {
