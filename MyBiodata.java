bf042bdf380c065239e737484ed93c58404a26d8
CresentCrysy <raymondevtant@gmail.com>
2024-09-04 16:54:55 +0700

    change to print

--- MyBiodata.java
+++ MyBiodata.java
@@ -1,8 +1,8 @@
 public class MyBiodata {
     public static void main(String[] args) {
-        String[] biodata= {"Raymon Devtant", "18", "Situbondo"};
+        String[] biodata= {"Raymon Devtant,", "18,", "Dari Situbondo"};
         for (int i=0; i<3; i++) {
-            System.out.println(biodata[i]);
+            System.out.print(biodata[i]+" ");
         }
     }
 }
